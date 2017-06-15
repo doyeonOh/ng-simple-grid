7d6745d62a0049615fbbfbb8eafa9a9128bac914
hodys <odyody12@gmail.com>
2017-06-15 22:20:43 +0900

    chore: export 에 gridComponent 추가 및 0.0.5 버전 업데이트

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { GridModule } from './app/grid/grid.module';
+import { GridComponent } from './app/grid/grid.component';
 export { GridOption, GridEvent, GridColumn } from './app/grid/grid.model';
 export { PaginationModule } from './app/pagination/pagination.module';
