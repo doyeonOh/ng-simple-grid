a04815dee700e422a1849d5919646b229537c86d
hodys <odyody12@gmail.com>
2017-06-17 17:40:19 +0900

    chore: tsc 로 빌드 후 배포 v0.0.8

--- dist/grid/grid.component.ngfactory.d.ts
+++
@@ -1,5 +0,0 @@
-import * as import1 from '@angular/core';
-import * as import3 from './grid.component';
-export declare const RenderType_GridComponent: import1.RendererType2;
-export declare function View_GridComponent_0(l: any): import1.ɵViewDefinition;
-export declare const GridComponentNgFactory: import1.ComponentFactory<import3.GridComponent>;
--- dist/pagination/pagination.component.js
+++ dist/pagination/pagination.component.js
@@ -1,3 +1,12 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 var PaginationComponent = (function () {
     function PaginationComponent() {
@@ -44,19 +53,25 @@ var PaginationComponent = (function () {
     };
     return PaginationComponent;
 }());
+__decorate([
+    Input(),
+    __metadata("design:type", Number)
+], PaginationComponent.prototype, "totalPageCount", void 0);
+__decorate([
+    Input(),
+    __metadata("design:type", Number)
+], PaginationComponent.prototype, "currentPageIndex", void 0);
+__decorate([
+    Output(),
+    __metadata("design:type", EventEmitter)
+], PaginationComponent.prototype, "movePage", void 0);
+PaginationComponent = __decorate([
+    Component({
+        selector: 'ng-simplePagination',
+        styleUrls: ["./pagination.component.scss"],
+        templateUrl: 'pagination.component.html'
+    }),
+    __metadata("design:paramtypes", [])
+], PaginationComponent);
 export { PaginationComponent };
-PaginationComponent.decorators = [
-    { type: Component, args: [{
-                selector: 'ng-simplePagination',
-                styleUrls: ["./pagination.component.scss"],
-                templateUrl: 'pagination.component.html'
-            },] },
-];
-/** @nocollapse */
-PaginationComponent.ctorParameters = function () { return []; };
-PaginationComponent.propDecorators = {
-    'totalPageCount': [{ type: Input },],
-    'currentPageIndex': [{ type: Input },],
-    'movePage': [{ type: Output },],
-};
 //# sourceMappingURL=pagination.component.js.map
