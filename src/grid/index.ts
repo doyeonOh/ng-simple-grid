6e0f67ac44163eebead5519b87c8f8f071150f36
hodys <odyody12@gmail.com>
2017-06-19 21:05:19 +0900

    v0.0.15

--- src/grid/index.ts
+++ src/grid/index.ts
@@ -1,3 +1,3 @@
-export { GridComponent } from './../../src/grid/grid.component';
-export { GridEvent, GridOption, GridColumn } from './../../src/grid/grid.model';
-export { GridModule } from './../../src/grid/grid.module';
+export { GridComponent } from './grid.component';
+export { GridEvent, GridOption, GridColumn } from './grid.model';
+export { GridModule } from './grid.module';
