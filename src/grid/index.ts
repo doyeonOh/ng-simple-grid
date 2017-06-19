f8cbf0f9e5c5b02a4875466c576c917521f12055
hodys <odyody12@gmail.com>
2017-06-19 20:49:19 +0900

    chore: v0.0.13

---
+++ src/grid/index.ts
@@ -0,0 +1,3 @@
+export { GridComponent } from './../../src/grid/grid.component';
+export { GridEvent, GridOption, GridColumn } from './../../src/grid/grid.model';
+export { GridModule } from './../../src/grid/grid.module';
