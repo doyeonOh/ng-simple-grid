b3ef397372eff9cf74a63153bb1313e18098cecb
hodys <odyody12@gmail.com>
2017-06-28 21:56:44 +0900

    chore: v0.0.23

--- src/grid/grid.module.ts
+++ src/grid/grid.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { GridComponent } from './grid.component';
 import { PaginationComponent } from './pagination.component';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule],
   declarations: [GridComponent, PaginationComponent],
   exports: [CommonModule, GridComponent, PaginationComponent]
 })
